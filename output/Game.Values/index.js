63ccc06ba52d6c25b22d32de79d260e5e477314d
Sid <siddharth.singh@juspay.in>
2018-03-09 19:48:34 +0530

    Remove ignored files

--- output/Game.Values/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var obstacleDist = 500;
-var groundPos = 200;
-var charCount = 3;
-module.exports = {
-    charCount: charCount,
-    obstacleDist: obstacleDist,
-    groundPos: groundPos
-};
